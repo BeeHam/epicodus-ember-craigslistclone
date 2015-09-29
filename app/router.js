b584f8e422203a010746e4e73e4998090f6d36a4
Tony Navarro & Brigham Thompson <student@epicodus.com>
2015-09-29 09:59:48 -0700

    list the details on unique listing route. Also will install the moment.js helper

--- app/router.js
+++ app/router.js
@@ -8,7 +8,7 @@ var Router = Ember.Router.extend({
 Router.map(function() {
   this.route('category', {path: '/category/:category_id'});
   this.route('admin');
-  this.route('listing');
+  this.route('listing', {path: '/listing/:listing_id'});
 });
 
 export default Router;
--- app/routes/listing.js
+++ app/routes/listing.js
@@ -2,9 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params) {
-    return Ember.RSVP.hash ({
-      category: this.store.findRecord('category', params.category_id),
-      listings: this.store.query('listing', {filter: {category: params.comment_id}})
-    });
+      return this.store.findRecord('listing', params.listing_id)
   }
+// a route for a single listing should only need a single model
 });
